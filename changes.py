5e0ab51016d372c952b96a3fe64c887d4b8e760f
azsi-exe <azsi@kent.ac.uk>
2026-02-04 14:57:17 +0000

    Line 13 = now it will stop printing once it hits 5, instead of an infinite loop.

--- changes.py
+++ changes.py
@@ -1,2 +1,3 @@
-# Line 91 = () for end of function
-# Line 28 = missing '='
+# Line 91 = () for end of function.
+# Line 28 = missing '='.
+# Line 13 = now it will stop printing once it hits 5, instead of an infinite loop.
